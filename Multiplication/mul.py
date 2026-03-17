05524820800b4505ff9546785bc72e21867a6a7a
ANORA\ranjith.n <Anora>
2026-03-17 14:06:51 +0530

    multiplication logic is completed

---
+++ Multiplication/mul.py
@@ -0,0 +1,8 @@
+def mul_to_numbers(num1: int , num2: int)->int:
+    """
+    This function multiply two numbers
+    :param num1:
+    :param num2:
+    :return: num1*num2
+    """
+    return num1*num2
