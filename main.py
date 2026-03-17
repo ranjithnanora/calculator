4244a5376d61294e9e2e6937c7fa3686f40289cc
ANORA\ranjith.n <Anora>
2026-03-17 14:44:39 +0530

    Added logic  to use Calculator in loop

--- main.py
+++ main.py
@@ -2,27 +2,32 @@ from Addition.add import add_two_number
 from Subtraction.sub import sub_to_numbers
 from Division.div import div_two_numbers
 from Multiplication.mul import mul_two_numbers
-print("Calculator")
-num1=int(input("Enter num One: "))
-num2=int(input("Enter num two: "))
-operation=input("Enter the operation(+,-,*,/): ")
-print(" = "*20)
-print(" "*15,"Calculator")
-print(" = "*20)
-match operation:
-    case add if add=="+":
-        print("Addition")
-        print("\t\t",num1,"+",num2,"=", add_two_number(num1,num2))
-    case sub if sub=="-":
-        print("subtraction")
-        print("\t\t",num1,"-",num2,"=",sub_to_numbers(num1,num2))
-    case mul if mul=="*":
-        print("Multiplication")
-        print("\t\t",num1,"*",num2,"=",mul_two_numbers(num1,num2))
-    case div if div=="/":
-        print("Division")
-        print("\t\t",num1,"/",num2,"=", div_two_numbers(num1,num2))
-    case _:
-        print("error: invalid operation")
 
-print(" = "*20)
+while True:
+    print("Enter Exit to quit")
+    num1=input("Enter num One: ")
+    if num1=="Exit":
+        break
+    num1=int(num1)
+    num2=int(input("Enter num two: "))
+    operation=input("Enter the operation(+,-,*,/): ")
+    print(" = "*20)
+    print(" "*15,"Calculator")
+    print(" = "*20)
+    match operation:
+        case add if add=="+":
+            print("Addition")
+            print("\t\t",num1,"+",num2,"=", add_two_number(num1,num2))
+        case sub if sub=="-":
+            print("subtraction")
+            print("\t\t",num1,"-",num2,"=",sub_to_numbers(num1,num2))
+        case mul if mul=="*":
+            print("Multiplication")
+            print("\t\t",num1,"*",num2,"=",mul_two_numbers(num1,num2))
+        case div if div=="/":
+            print("Division")
+            print("\t\t",num1,"/",num2,"=", div_two_numbers(num1,num2))
+        case _:
+            print("error: invalid operation")
+
+    print(" = "*20)
