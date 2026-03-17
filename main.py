4ec0d0b59d72d849e4e87cd38a9d5d04ab61c7aa
ANORA\ranjith.n <Anora>
2026-03-17 12:47:56 +0530

    fixed int bug and added logic to check  the operation type

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 print("Calculator")
-num1=input(int("Enter num One: "))
-num2=input(int("Enter num two: "))
+num1=int(input("Enter num One: "))
+num2=int(input("Enter num two: "))
 operation=input("Enter the operation(+,-,*,/): ")
+
+match operation:
+    case add if add=="+":
+        print("Addition")
+    case sub if sub=="-":
+        print("subtraction")
+    case mul if mul=="*":
+        print("Multiplication")
+    case div if div=="/":
+        print("Division")
+    case _:
+        print("error: invalid operation")
