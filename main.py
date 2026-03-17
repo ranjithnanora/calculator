1cd4ee0bf1bd588a518d3645feea53fc3c548852
ANORA\ranjith.n <Anora>
2026-03-17 14:23:15 +0530

    modified the terminal output style

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from Addition.add import add_two_number
 from Subtraction.sub import sub_to_numbers
-print("Calculator")
+
 num1=int(input("Enter num One: "))
 num2=int(input("Enter num two: "))
 operation=input("Enter the operation(+,-,*,/): ")
-
+print(" = "*20)
+print(" "*15,"Calculator")
+print(" = "*20)
 match operation:
     case add if add=="+":
         print("Addition")
-        print(num1,"+",num2,"=", add_two_number(num1,num2))
+        print("\t\t",num1,"+",num2,"=", add_two_number(num1,num2))
     case sub if sub=="-":
         print("subtraction")
-        print(num1,"-",num2,"=",sub_to_numbers(num1,num2))
+        print("\t\t",num1,"-",num2,"=",sub_to_numbers(num1,num2))
     case mul if mul=="*":
         print("Multiplication")
     case div if div=="/":
         print("Division")
     case _:
         print("error: invalid operation")
+
+print(" = "*20)
