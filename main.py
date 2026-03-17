e08c4faa9842c6a8147216b77efb03f581af0690
ANORA\ranjith.n <Anora>
2026-03-17 14:09:06 +0530

    applied multiplication logic in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from Multiplication.mul import mul_two_numbers
 print("Calculator")
 num1=int(input("Enter num One: "))
 num2=int(input("Enter num two: "))
@@ -10,6 +11,7 @@ match operation:
         print("subtraction")
     case mul if mul=="*":
         print("Multiplication")
+        print(num1,"*",num2,"=",mul_two_numbers(num1,num2))
     case div if div=="/":
         print("Division")
     case _:
